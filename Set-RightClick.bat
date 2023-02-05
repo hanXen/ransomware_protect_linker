73cd89aa53531959adba814254eac0e770e2c526
hanxen <shl9798@korea.ac.kr>
2023-02-05 23:48:07 +0900

    feat: Right-click function for usability

--- Set-RightClick.bat
+++ Set-RightClick.bat
@@ -7,4 +7,4 @@ reg add "HKEY_CLASSES_ROOT\*\shell\runas\command" /t REG_SZ /v "" /d "\"%hiding_
 reg add "HKEY_CLASSES_ROOT\*\shell\Recover File (Extract File)" /t REG_SZ /v "" /d "" /f
 reg add "HKEY_CLASSES_ROOT\*\shell\Recover File (Extract File)\command" /t REG_SZ /v "" /d "\"%recovery_path%\" --recoverfile \"%%3\"" /f
 
-pause
+pause 
