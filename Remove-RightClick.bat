387703912ccca3402a5e40a1b0c3ef8b888fede6
hanxen <shl9798@korea.ac.kr>
2025-04-14 15:22:58 +0900

    chore: add script for clean up registry configurations

---
+++ Remove-RightClick.bat
@@ -0,0 +1,5 @@
+reg delete "HKEY_CLASSES_ROOT\*\shell\runas" /f
+reg delete "HKEY_CLASSES_ROOT\*\shell\runas\command" /f
+
+reg delete "HKEY_CLASSES_ROOT\*\shell\Recover File (Extract File)" /f
+reg delete "HKEY_CLASSES_ROOT\*\shell\Recover File (Extract File)\command" /f
