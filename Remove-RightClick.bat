c42d08a2e364f0f630001f2b5ee82857549cdf47
hanxen <shl9798@korea.ac.kr>
2025-04-25 13:24:30 +0900

    chore: update right-click menu for file hiding with security improvements

--- Remove-RightClick.bat
+++ Remove-RightClick.bat
@@ -1,5 +1,11 @@
-reg delete "HKEY_CLASSES_ROOT\*\shell\runas" /f
-reg delete "HKEY_CLASSES_ROOT\*\shell\runas\command" /f
+@echo off
+
+echo.
+echo "[*] Removing registry entries for Hide File (Create lnk File) and Recover File (Extract File)..." 
+echo.
+
+reg delete "HKEY_CLASSES_ROOT\*\shell\Hide File" /f
+reg delete "HKEY_CLASSES_ROOT\*\shell\Hide File\command" /f
 
 reg delete "HKEY_CLASSES_ROOT\*\shell\Recover File (Extract File)" /f
 reg delete "HKEY_CLASSES_ROOT\*\shell\Recover File (Extract File)\command" /f
