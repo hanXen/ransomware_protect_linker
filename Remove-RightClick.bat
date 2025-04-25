6264a36d2e99d0a6eb79cf42a0f4dd0e622c22e5
hanxen <shl9798@korea.ac.kr>
2025-04-25 15:28:04 +0900

    chore: simplify batch file to remove registry entries

--- Remove-RightClick.bat
+++ Remove-RightClick.bat
@@ -5,7 +5,6 @@ echo "[*] Removing registry entries for Hide File (Create lnk File) and Recover 
 echo.
 
 reg delete "HKEY_CLASSES_ROOT\*\shell\Hide File" /f
-
 reg delete "HKEY_CLASSES_ROOT\Lnkfile\shell\Recover File" /f
 
 pause
