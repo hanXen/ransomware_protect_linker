9b4f46408fcdeb2eb110fe61b3ed54ea9f16e38b
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2025-04-27 18:59:35 +0530

    feat: Support cross-drive file hiding & recovery with administrative privileges

--- install.ps1
+++ install.ps1
@@ -27,7 +27,7 @@ if (Test-Path "dist") {
 }
 
 uv run pyinstaller -F hiding.py --uac-admin --manifest admin.manifest
-uv run pyinstaller -F recovery.py
+uv run pyinstaller -F recovery.py --uac-admin --manifest admin.manifest
 uv run pyinstaller -F linker.py --uac-admin --manifest admin.manifest
 echo "`n---------------------------------`n"
 
